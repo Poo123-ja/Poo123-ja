1ef45f88e61b3bbb1a5b4b9fcb127e76dc49ec30
Poo123-ja <100204604+Poo123-ja@users.noreply.github.com>
2022-03-07 23:37:51 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,8 @@
+a=float(input("enter the first angle of triangle"))
+b=float(input("enter the second angle of triangle"))
+c=float(input("enter the third angle of triangle"))
+total=a+b+c
+if(total==180):
+  print("valid triangle")
+else:
+  print("invalid triangle")
