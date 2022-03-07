aeff9133af9e6e99c058150379fce06520e83f86
Poo123-ja <100204604+Poo123-ja@users.noreply.github.com>
2022-03-07 20:18:50 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the number"))
+if(n%5==0):
+  print("The number is divisible by 5")
+elif(n%11==0):
+  print("The number is divisible by 11")
+else:
+  print("The number is not divisible by 5 and 11")
+  
