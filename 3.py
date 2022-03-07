21bddc087b9f7145af079986240e2438465fce88
Poo123-ja <100204604+Poo123-ja@users.noreply.github.com>
2022-03-07 19:54:58 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+if(num1>num2):
+  print("num1,"greater)
+elif(num2>num1):
+  print("num2,"greater)
+else:
+  print("both are equal")
